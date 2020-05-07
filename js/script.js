8817465eba8877546975586d76c4738a46d324fe
SAHIL SHAHANE <57249267+sahilbest999@users.noreply.github.com>
2020-05-07 17:14:43 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-let file_path;
-let inp_e = document.createElement('input');
+let inp_e = document.createElement('input');;
 inp_e.setAttribute('type','file');
 inp_e.id='add_img';
 inp_e.setAttribute('accept','image/jpeg,image/png');
 inp_e.multiple=true;
 
+let img_data;
+let elementRef;
+let gallery;
+
+let div;
+let img;
+let span;
+
 let images=[];
+const meta_data = [];
 let length=0;
+
 
 $('#upload').on('click',() => inp_e.click());
 
@@ -17,10 +26,50 @@ $(inp_e).change(function (e) {
         for(let obj of e.target.files)
         {
             images.push(obj);
-            $("#img_frames").append('<div class="grid-item" id = "imgdiv_'+length+'"><img class="img_data_mini" src="#" id="img_'+length+'"></div>'); 
-            let image = document.getElementById('img_'+length);
-            image.src = URL.createObjectURL(obj);
+
+            div = document.createElement("div");
+          //  div.className = "grid-item";
+          div.classList.add(...["column"]);
+            div.id = `imgdiv_${length}`;
+
+            img = document.createElement("img");
+            img.src= URL.createObjectURL(obj);
+            img.className="img_data_mini";
+            img.id=length;
+            img.onload = () => URL.revokeObjectURL(obj);
+
+            $("#img_frames").append(img);
+            
+            meta_data.push(getMeta_data(obj));
             ++length;
+            // img_data = URL.createObjectURL(obj);
+            // URL.revokeObjectURL(obj)
+            // gallery.addItems({
+            //     thumbnailSrc: img_data, // link to thumbnail image
+            //     enlargedSrc: img_data, // link to enlarged image
+            //     enlargedWidth: 300,
+            //     enlargedHeight: 300,
+            //     title: obj.name, // Title for the label or button
+            //     link: img_data
+            // });
         }
     }
 });
+
+function getMeta_data(obj){
+    return {
+        size : obj.size,
+        name : obj.name,
+        lastModified : obj.lastModified,
+    }
+}
+
+$($(".img_data_mini").first()).dblclick(()=> alert(`Name : ${meta_data[$(this).id].name}\nSize : ${meta_data[$(this).id].size}\nLastModified : ${new Date(meta_data[$(this).id].lastModified)}`));
+
+// window.addEventListener('load', function() {
+//     elementRef = document.getElementById('gallery');
+
+//     gallery = new NaturalGallery.Natural(elementRef); // or
+   
+//     gallery.init();
+// }); 
